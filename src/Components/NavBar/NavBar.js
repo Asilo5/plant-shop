9b414ba052a64fba1e6fa5de4495c1caf9cf2c4c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-09 23:25:39 -0700

    Add NavLink to NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './NavBar.scss';
+import { NavLink } from 'react-router-dom'
 
 import Search from '../../Containers/Search/Search';
 
 const NavBar  = () => {
 
-    return (
+    return ( 
         <nav>
            <h1>Plant Shop</h1>
 
            <div>
                <Search />
-               <p>Add Plant</p>
-               <p>Loved</p>
+               <NavLink to='/addPlant'>Add Plant </NavLink>
+               <NavLink to='/lovedPlant'>Loved Plant</NavLink>
            </div>
         </nav>
     )
