fbc6637ac17d8e4230b0822e90af0848c035f8d9
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-09 21:37:52 -0700

    Add NavBar files

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar  = () => {
+
+    return (
+        <nav>
+           <h1>Plant Shop</h1>
+
+           
+        </nav>
+    )
+}
+
+export default NavBar;
--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import  { getPlants } from '../../apiCalls';
-import NavBar from '../../Components/NavBar';
+import NavBar from '../../Components/NavBar/NavBar';
 
 class App extends Component  {
    
