72f32f1313ab7b697b2fcd3b6bafddf6823d678b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-09 21:46:55 -0700

    Add NavBar info needed inside

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import Search from '../../Containers/Search';
+
 const NavBar  = () => {
 
     return (
         <nav>
            <h1>Plant Shop</h1>
 
-           
+           <div>
+               <Search />
+               <p>Add Plant</p>
+               <p>Loved</p>
+           </div>
         </nav>
     )
 }
