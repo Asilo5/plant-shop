9652c30c5fcf3a329633d6c4a516390d0de83e9e
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-11 14:24:01 -0700

    Add routes

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -4,7 +4,12 @@ import  { getPlants } from '../../apiCalls';
 import { addPlants, hasError } from '../../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { NavLink, Route } from 'react-router-dom';
+
 import NavBar from '../../Components/NavBar/NavBar';
+import PlantsContainer from '../PlantsContainer';
+import AddPlants from '../AddPlants';
+import FavouritesContainer from '../FavouritesContainer';
 
 export class App extends Component  {
    
@@ -19,8 +24,15 @@ export class App extends Component  {
     return (
       <main>
          <NavBar />
+         <Route exact path='/' component={Header} />
+         <Route exact path='/allPlants' component={PlantsContainer} />
+         <Route exact path='/addPlants' component={AddPlants} />
+         <Route exact path='/favourites' component={FavouritesContainer} />
          <header>
-             <h2> Plan(t) your Garden!</h2>
+            <section>
+              <h2> Plan(t) your Garden!</h2>
+              <NavLink to='/allplants'>SEE OUR PLANTS</NavLink>
+            </section>
          </header>
       </main>
     )
