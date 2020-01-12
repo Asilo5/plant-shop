66b5aed165503b51c11174562ef11d483179ade5
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-11 22:17:34 -0700

    Add snapshot test to Pant component

---
+++ src/Components/Plant/Plant.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Plant from './Plant';
+
+describe('Plant component', () => {
+    it('should match snapshot', () => {
+        let wrapper = shallow(<Plant _id={1} 
+                                     name='Aloe'
+                                     price={15} 
+                                     image='image link' />)
+        expect(wrapper).toMatchSnapshot();
+    })
+})
