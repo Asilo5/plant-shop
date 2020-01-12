a1bb20f60ae786653d96646eb9ac84a888940c4e
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-11 17:00:50 -0700

    Add mapStateToProps to PlantsContainer

--- src/Containers/PlantsContainer/PlantsContainer.js
+++ src/Containers/PlantsContainer/PlantsContainer.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './PlantsContainer.scss';
+import { connect } from 'react-redux';
 
-const PlantsContainer = () => {
+export const PlantsContainer = ({ allPlants }) => {
+   let plantsList = allPlants.map((plant) => {
+     return 
+   });
 
     return(
       <section>
           <div className='plants-header'>
             <h2>Our Plants</h2>
           </div>
+          {plantsList}
       </section>
     )
 }
 
-export default PlantsContainer;
+export const mapStateToProps = (state) => ({
+    allPlants: state.allPlants
+})
+
+export default connect(mapStateToProps)(PlantsContainer);
