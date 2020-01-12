0ad5b997167599db0ef05804345e574f67c0fef2
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-12 16:56:43 -0700

    Add newplant case to allPlants

--- src/reducers/allPlants.js
+++ src/reducers/allPlants.js
@@ -2,6 +2,8 @@ export const allPlants = ( state = [], action ) => {
     switch(action.type) {
        case 'ADD_PLANTS':
            return action.setPlants;
+       case 'NEW_PLANT':
+           return [...state, action.newPlant]
         default:
             return state;
     }
