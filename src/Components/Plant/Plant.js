b81570846a73d520e494cc6ea81abf0832511aa7
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-13 13:58:16 -0700

    Update plant to class

--- src/Components/Plant/Plant.js
+++ src/Components/Plant/Plant.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Plant.scss';
 import { Link } from 'react-router-dom';
 import { FaSeedling } from 'react-icons/fa';
  
-const Plant = ({_id, name, price, image}) => {
+class Plant extends Component {
 
-    return (
-      <section className='plant-info'>
-          <Link to={(`/plant/${_id}`)}>
-            <img src={image} alt={name} />
-            <div className='plant-name'>
-                <div>
-                    <p>{name}</p>
-                    <p>£{price}</p>
-                </div>
-                <button><FaSeedling/></button>
-            </div>
-          </Link>
-      </section>
-    )
+ 
+
+ render () {
+  const {_id, name, price, image} = this.props;
+  return (
+    <section className='plant-info'>
+        <Link to={(`/plant/${_id}`)}>
+          <img src={image} alt={name} />
+          <div className='plant-name'>
+              <div>
+                  <p>{name}</p>
+                  <p>£{price}</p>
+              </div>
+              <button><FaSeedling/></button>
+          </div>
+        </Link>
+    </section>
+  )
+
+ }
 }
 
 export default Plant;
