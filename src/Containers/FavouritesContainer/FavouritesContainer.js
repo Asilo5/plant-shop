1fd524d9896ac45eb8f97183ceb217d798c2b503
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-13 13:22:10 -0700

    Add style to favourites container

--- src/Containers/FavouritesContainer/FavouritesContainer.js
+++ src/Containers/FavouritesContainer/FavouritesContainer.js
@@ -5,15 +5,27 @@ import Plant from '../../Components/Plant/Plant';
 
 
 export const FavouritesContainer = ({favouritePlants, allPlants}) => {
-    const findPlants = favouritePlants.map((plantID) => {
-        return allPlants.find((plant) => plant._id === plantID)
+
+    const lovePlant = allPlants.map((plant) => {
+       if(favouritePlants.includes(plant._id)) {
+         return <Plant {...plant} />
+       }
     });
 
-    const lovedPlant = findPlants.map((plant) => <Plant {...plant} />)
+    // const findPlants = favouritePlants.map((plantID) => {
+    //     return allPlants.find((plant) => plant._id === plantID)
+    // });
+
+    // const lovedPlant = findPlants.map((plant) => <Plant {...plant} />)
 
     return (
-        <section>
-           {favouritePlants.length === 0 ? <h2>ADD YOUR MOST LOVED PLANTS FIRST</h2> : {lovedPlant}}
+        <section className='fav-container'>
+            <section className='fav-header'>
+                <h2>Your Loved Plants</h2>
+            </section>
+            <section className='fav-section'>
+                {favouritePlants.length === 0 ? <h3>ADD YOUR MOST LOVED PLANTS FIRST</h3> : {lovePlant}}
+            </section>
         </section>
     )
 }
